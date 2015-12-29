43d0ebc9e14c0371abd0ce5d89484d4926a88d01
Krishna Yalamanchili <chaitu.jil@gmail.com>
2015-12-29 15:52:40 -0600

    cleanup

--- www/js/vibes/channels/ChannelDetailCtrl.js
+++ www/js/vibes/channels/ChannelDetailCtrl.js
@@ -1,46 +1,53 @@
-controllers.controller('ChannelDetailCtrl', function ($rootScope, $scope, $stateParams, $timeout, ngXml2json, ChannelsService, HttpService) {
+(function () {
+    'use strict';
 
-    var audio;
+    angular.module('vibes')
+        .service('ChannelDetailCtrl', channelDetailCtrl);
 
-    init();
-    currentSongCtrl();
+    function channelDetailCtrl ($rootScope, $scope, $stateParams, $timeout, ngXml2json, ChannelsService, HttpService) {
 
-    function init() {
-        $rootScope.playing = false;
-        $scope.channel = ChannelsService.get($stateParams.channelId);
+        var audio;
+
+        init();
         currentSongCtrl();
 
-        audio = document.getElementById('audio');
+        function init() {
+            $rootScope.playing = false;
+            $scope.channel = ChannelsService.get($stateParams.channelId);
+            currentSongCtrl();
+
+            audio = document.getElementById('audio');
+        }
+
+        $scope.play = function () {
+            audio.play();
+            $rootScope.playing = true;
+        };
+
+        $scope.pause = function () {
+            audio.pause();
+            $rootScope.playing = false;
+        };
+
+        function currentSongCtrl() {
+            (function tick() {
+                HttpService.getCurrentSongInfo($scope.channel.radioUuid, $scope.channel.apiKey).then(function (response) {
+                    var currentSongInfo = ngXml2json.parser(response);
+                    $scope.songTitle = currentSongInfo.tracks.track.title;
+                    $scope.songArtists = currentSongInfo.tracks.track.artists;
+                    if (typeof currentSongInfo.tracks.track.cover !== 'undefined'
+                        && currentSongInfo.tracks.track.cover !== null
+                        && currentSongInfo.tracks.track.cover.length > 1) {
+                        $scope.songCover = currentSongInfo.tracks.track.cover;
+                    } else {
+                        $scope.songCover = 'http://www.musicheavens.com/wp-content/gallery/music-photo/beautiful-guitar-guitar-music-1920x1080.jpg';
+                    }
+
+                    // Calling back tick
+                    $timeout(tick, (currentSongInfo.tracks.track.callmeback));
+                });
+            })();
+        }
+
     }
-
-    $scope.play = function () {
-        audio.play();
-        $rootScope.playing = true;
-    };
-
-    $scope.pause = function () {
-        audio.pause();
-        $rootScope.playing = false;
-    };
-
-    function currentSongCtrl() {
-        (function tick() {
-            HttpService.getCurrentSongInfo($scope.channel.radioUuid, $scope.channel.apiKey).then(function (response) {
-                var currentSongInfo = ngXml2json.parser(response);
-                $scope.songTitle = currentSongInfo.tracks.track.title;
-                $scope.songArtists = currentSongInfo.tracks.track.artists;
-                if (typeof currentSongInfo.tracks.track.cover !== 'undefined'
-                    && currentSongInfo.tracks.track.cover !== null
-                    && currentSongInfo.tracks.track.cover.length > 1) {
-                    $scope.songCover = currentSongInfo.tracks.track.cover;
-                } else {
-                    $scope.songCover = 'http://www.musicheavens.com/wp-content/gallery/music-photo/beautiful-guitar-guitar-music-1920x1080.jpg';
-                }
-
-                // Calling back tick
-                $timeout(tick, (currentSongInfo.tracks.track.callmeback));
-            });
-        })();
-    }
-
-});
+})();
--- www/js/vibes/channels/ChannelsCtrl.js
+++ www/js/vibes/channels/ChannelsCtrl.js
@@ -1,3 +1,10 @@
-controllers.controller('ChannelsCtrl', function($scope, ChannelsService) {
-    $scope.channels = ChannelsService.all();
-});
+(function () {
+    'use strict';
+
+    angular.module('vibes')
+        .service('ChannelsCtrl', channelsCtrl);
+
+    function channelsCtrl($scope, ChannelsService) {
+        $scope.channels = ChannelsService.all();
+    }
+})();
