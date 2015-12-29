44f1397e7fe5e8012b1dac185948374d895499b8
Krishna Yalamanchili <chaitu.jil@gmail.com>
2015-12-29 14:57:39 -0600

    Changed structure of js files and code to follow johnpapa angular style guide. More changes to come

---
+++ www/js/vibes/login/LoginCtrl.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    angular.module('vibes')
+        .controller('LoginCtrl', loginCtrl);
+
+    function loginCtrl($scope, LoginService, $ionicPopup, $state) {
+        $scope.data = {};
+
+        $scope.login = function() {
+            LoginService.loginUser($scope.data.username, $scope.data.password).success(function (data) {
+                $state.go('tab.channels');
+            }).error(function(data) {
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Login failed!',
+                    template: 'Please check your credentials!'
+                });
+            });
+        }
+    }
+})();
