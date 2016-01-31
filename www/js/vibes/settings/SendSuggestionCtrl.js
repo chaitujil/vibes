097e099d16eabb4010f459405fdf0f2534d5b098
Krishna Yalamanchili <chaitu.jil@gmail.com>
2016-01-30 20:04:58 -0600

    Added logic to send emails. Not working though.

--- www/js/vibes/settings/SendSuggestionCtrl.js
+++ www/js/vibes/settings/SendSuggestionCtrl.js
@@ -11,9 +11,22 @@
 
         vm.sendSuggestion = sendSuggestion;
 
-        function sendSuggestion (suggestion) {
+        function sendSuggestion(suggestion) {
             if (typeof suggestion !== "undefined" && suggestion.length > 0) {
-                // TODO send suggestion as an email
+                if (window.plugins && window.plugins.emailComposer) {
+                    alert("window plugins not null");
+                    window.plugins.emailComposer.showEmailComposerWithCallback(function (result) {
+                            alert("Email successfully sent " + result);
+                        },
+                        "Suggestion from user", // Subject
+                        suggestion,                      // Body
+                        ["chaitu.jil@gmail.com"],    // To
+                        null,                    // CC
+                        null,                    // BCC
+                        false,                   // isHTML
+                        null,                    // Attachments
+                        null);                   // Attachment Data
+                }
                 $log.debug(suggestion);
                 alert("Message sent");
             }
