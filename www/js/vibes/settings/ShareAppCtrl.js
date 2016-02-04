ef8878733f6cb83e2d64e71c13fc8f6d3ddb554a
Krishna Yalamanchili <chaitu.jil@gmail.com>
2016-02-04 00:19:48 -0600

    Fixed page load issue

--- www/js/vibes/settings/ShareAppCtrl.js
+++ www/js/vibes/settings/ShareAppCtrl.js
@@ -4,29 +4,17 @@
     angular.module('vibes')
         .controller('ShareThisAppCtrl', ShareThisAppCtrl);
 
-    ShareThisAppCtrl.$inject = ['$cordovaSocialSharing'];
+    ShareThisAppCtrl.$inject = [];
 
-    function ShareThisAppCtrl($cordovaSocialSharing) {
+    function ShareThisAppCtrl() {
         var vm = this;
         vm.shareViaTwitter = shareViaTwitter;
         vm.shareViaFacebook = shareViaFacebook;
 
         function shareViaTwitter() {
-            $cordovaSocialSharing
-                .shareViaTwitter('Message via Twitter', null /* img */, null /* url */)
-                .then(function (result) {
-                    console.log('share ok');
-                }, function (err) {
-                    alert(err)
-                });
         }
 
         function shareViaFacebook() {
-            window.plugins.socialsharing.shareViaFacebook('Message via Facebook', null /* img */, null /* url */, function () {
-                console.log('share ok');
-            }, function (errormsg) {
-                alert(errormsg)
-            });
         }
     }
 })();
