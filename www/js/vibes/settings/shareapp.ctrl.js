2a3f8f72a804ad412802c0e8139d91f7fb6ff448
Krishna Yalamanchili <chaitu.jil@gmail.com>
2016-05-21 18:19:02 -0500

    Added share this app implementation.

--- www/js/vibes/settings/shareapp.ctrl.js
+++ www/js/vibes/settings/shareapp.ctrl.js
@@ -4,17 +4,36 @@
     angular.module('vibes')
         .controller('ShareThisAppCtrl', ShareThisAppCtrl);
 
-    ShareThisAppCtrl.$inject = [];
+    ShareThisAppCtrl.$inject = ['$cordovaSocialSharing'];
 
-    function ShareThisAppCtrl() {
+    function ShareThisAppCtrl($cordovaSocialSharing) {
         var vm = this;
         vm.shareViaTwitter = shareViaTwitter;
         vm.shareViaFacebook = shareViaFacebook;
+        vm.shareViaWhatsApp = shareViaWhatsApp;
 
         function shareViaTwitter() {
+            window.plugins.socialsharing.shareViaTwitter (
+                null,
+                null /* img */,
+                'https://play.google.com/store/apps/details?id=com.ionicframework.example146317' /* url */
+            );
         }
 
         function shareViaFacebook() {
+            window.plugins.socialsharing.shareViaFacebook(
+                null,
+                null /* img */,
+                'https://play.google.com/store/apps/details?id=com.ionicframework.example146317' /* url */
+            );
+        }
+
+        function shareViaWhatsApp() {
+            window.plugins.socialsharing.shareViaWhatsApp(
+                'Message via WhatsApp',
+                null /* img */,
+                'https://play.google.com/store/apps/details?id=com.ionicframework.example146317' /* url */
+            );
         }
     }
 })();
